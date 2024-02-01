617d2ab27e1f72151a5c9159ec414b3a6b7c023c
peacefullyquietly <pjh73015@gmail.com>
2024-02-01 12:10:58 +0900

    24.01.23

---
+++ JH/15641.py
@@ -0,0 +1,20 @@
+# N과 M(1)
+def backtracking(N, M, result, visited):
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+
+    for i in range(1, N + 1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            backtracking(N, M, result, visited)
+            result.pop()
+            visited[i] = False
+
+
+N, M = map(int, input().split())
+visited = [False] * (N + 1)
+result = []
+
+backtracking(N, M, result, visited)
