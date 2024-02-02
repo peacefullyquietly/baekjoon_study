013f1a1b19cf4bc9341509749da959593cd5ced9
hyemin <hyemin6647@naver.com>
2024-02-03 02:39:36 +0900

    2024년 2월 3일
    약수, 배수와 소수

---
+++ HM/2501.py
@@ -0,0 +1,29 @@
+# 약수 구하기
+
+N , K = map(int, input().split())
+
+count = 0 # 약수 순서
+
+for i in range(1, N+1):
+
+    if N%i==0:
+        count+=1
+    if count==K:
+        print(i)
+        break
+else: print(0)
+
+
+# 다른 풀이
+# arr=[]
+# N, K = map(int, input().split())
+# cnt = 0
+# for i in range(1, N + 1):
+#     if N % i == 0:
+#         arr.append(i)
+#     cnt += 1
+
+# if K > len(arr):
+#     print(0)
+# else:
+#     print(arr[K-1])
