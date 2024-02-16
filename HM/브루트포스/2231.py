2c8b8c90aac69dbcb4eeb6f9f72c0ca472ee3e75
hyemin <hyemin6647@naver.com>
2024-02-16 21:23:26 +0900

    2024년 2월 16일

--- HM/브루트포스/2231.py
+++ HM/브루트포스/2231.py
@@ -1,11 +1,11 @@
-N = int(input())
-list =[]
-for i in range(10):
-    for j in range(10):
-        for k in range(10):
-            if i*100+j*10+k+i+j+k==N:
-                list.append(i*100+j*10+k)
-print(min(list))
+# N = int(input())
+# list =[]
+# for i in range(10):
+#     for j in range(10):
+#         for k in range(10):
+#             if i*100+j*10+k+i+j+k==N:
+#                 list.append(i*100+j*10+k)
+# print(min(list))
 
                 
 # N = int(input())
@@ -19,4 +19,22 @@ print(min(list))
       
 # print(sum)
        
-       
+
+N = int(input())
+total=0
+for i in range(1, N):
+    nums =list(str(i))
+    num = list(map(int,nums))
+    # print(num)
+    # print(sum(num))
+    total=sum(num)+i
+
+    if total==N:
+        print(i)
+        break
+else:
+    print(0)        
+   
+    # if sum(num)+i==N:
+    #     print(i)
+# print(sum([1,3,4]))
