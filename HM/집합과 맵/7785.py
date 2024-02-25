ec3f80d25f5c3c01bef21e74cd19bdae54afddfa
hyemin <hyemin6647@naver.com>
2024-02-25 14:12:55 +0900

    2024년 2월 25일

--- HM/집합과 맵/7785.py
+++ HM/집합과 맵/7785.py
@@ -1,13 +1,28 @@
+# N = int(input())
+# enter=set()
+# leave=set()
+# for  i in range(N):
+#     name,status =input().split() 
+#     if status=='enter':
+#         enter.add(name)
+#     else:leave.add(name)
+    
+# enter = enter-leave
+# enter=sorted(enter, reverse=True)
+
+# for i in enter:
+#     print(i)
+
 N = int(input())
 enter=set()
-leave=set()
+
 for  i in range(N):
     name,status =input().split() 
     if status=='enter':
         enter.add(name)
-    else:leave.add(name)
+    else:enter.remove(name)
     
-enter = enter-leave
+
 enter=sorted(enter, reverse=True)
 
 for i in enter:
